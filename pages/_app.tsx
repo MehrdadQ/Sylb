e546236895d499b8fd3d95cfe2cfd0ff7121c4a4
Mehrdad Ghannad <mehrdad37ghannad@gmail.com>
2023-08-11 01:12:00 -0400

    added user authentication check before accessing certain pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import { RecoilRoot } from 'recoil';
 import { createGlobalStyle } from 'styled-components';
 import '../styles/globals.css';
+import { auth } from '../utilities/firebase';
+import LoadingIcon from "../public/loading.svg";
+import Image from 'next/image';
+
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -12,6 +18,29 @@ const GlobalStyle = createGlobalStyle`
 `
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (!user && !['/', '/login', '/signup'].includes(router.pathname)) {
+        router.push('/login');
+      } else {
+        setIsLoading(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, [router]);
+
+  if (isLoading) {
+    return (
+      <div style={{display: "flex", width: "100%", height: "100vh", justifyContent: "center"}}>
+        <Image src={LoadingIcon} alt='loading' style={{width: "48px", height: 'auto'}}/>
+      </div>
+    )
+  }
+
   return (
     <RecoilRoot>
       <GlobalStyle/>
